428d18efe7f76cec51fbb9a1b079c03f3112e218
fritz-smh <fritz.smh@domogik.org>
2014-03-11 14:19:33 +0100

    update

--- install.sh
+++ install.sh
@@ -1,19 +1,14 @@
-#!/bin/bash
-#
-# this script is to put in crontab!
-# it will create (if needed) or update the dashbord
-
-
 BASE_DIR=/var/www/repo/
 
 if [ -d /var/www/repo/dashboard/.git/ ] ; then
     # pull the repo
     cd $BASE_DIR/dashboard/
     git pull
+    chown -R www-data:www-data $BASE_DIR/dashboard/
 else
     # clone the repo
     cd $BASE_DIR
     git clone https://github.com/fritz-smh/dashboard.git
-    chown www-data:www-data $BASE_DIR/dashboard/
+    chown -R www-data:www-data $BASE_DIR/dashboard/
 fi
 
